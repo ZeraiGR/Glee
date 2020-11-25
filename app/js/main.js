cd4ff2465f9df1772f9747767a35b30cc9274cd8
ZeRaiGR <kirillgr15@mail.ru>
2020-11-25 20:47:49 +0300

    added design

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,15 @@ $(function (params) {
 	});
 
 
-	var mixer = mixitup('.categories__products');
+	var containerEl1 = document.querySelector('[data-ref="container-1"]');
+	var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+	var config = {
+		controls: {
+			scope: 'local',
+		}
+	};
+
+	var mixer1 = mixitup(containerEl1, config);
+	var mixer2 = mixitup(containerEl2, config);
 });

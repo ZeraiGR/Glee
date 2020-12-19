b789edacf04b61b3dfc3896f552bb04f2224a7e1
ZeRaiGR <kirillgr15@mail.ru>
2020-12-19 11:43:36 +0300

    adaptiv home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,38 @@
 $(function (params) {
 
+	$('.partners__list').slick({
+		slidesToShow: 5,
+		responsive: [{
+				breakpoint: 979,
+				settings: {
+					slidesToShow: 4,
+					arrows: false,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					arrows: false,
+				}
+			},
+			{
+				breakpoint: 414,
+				settings: {
+					slidesToShow: 2,
+					arrows: false,
+				}
+			},
+			{
+				breakpoint: 380,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+				}
+			},
+		]
+	});
+
 	$('.header__btn').on('click', function () {
 		$('.menu__list').toggleClass('open');
 	});
